d91ece7dd2b0ce97bbacd8d2d61010b5ac74ceee
Cesar Augusto <cachxd1@gmail.com>
2022-06-14 21:26:41 -0500

    Se agregaron nuevas funciones en configurar.php y se eliminaron elementos depreciados o se reemplazaron por nuevas funciones. Se creo un archivo 'configurar.user.php' para agregar todas las configuraciones adicionales que necesite aplicar para la plataforma

--- Config/configurar.php
+++ Config/configurar.php
@@ -1,5 +1,6 @@
 <?php
- //Configuración de acceso a la base de datos
+date_default_timezone_set("America/Bogota");
+//Configuración de acceso a la base de datos
 //Local
 define("HOST", "localhost");
 define("USERNAME", "root");
@@ -7,25 +8,33 @@ define("PASSWORD", "");
 define("DATABASE", "");
 define("LOCALDIR", "Framework_PHP/");
 
+define("LANG", "es");
+define("IS_SSL", true);
+define("CLAVE_RECAPTCHA_V3", "");
+
 //Ruta app
 define("RUTA_APP", dirname(dirname(__FILE__)) . "/");
 //Ruta url
-if ($_SERVER["SERVER_PORT"] == 80) { //Puerto por defecto
-    $aux =  "http://" . $_SERVER['SERVER_NAME'] . LOCALDIR;
-} else { //Puerto personalizado
-    $aux = "http://" . $_SERVER['SERVER_NAME'] . ":" . $_SERVER["SERVER_PORT"] . LOCALDIR;
+$http = "http" . (IS_SSL ? "s" : "");
+$aux =  $http . "://" . $_SERVER['SERVER_NAME'];
+if (!($_SERVER["SERVER_PORT"] == 80 || $_SERVER["SERVER_PORT"] == 443)) { //Puerto personalizado
+    $aux .= ":" . $_SERVER["SERVER_PORT"] . LOCALDIR;
 }
+$aux .= LOCALDIR;
 define("RUTA_URL", $aux);
 //Nombre del sitio
 define("NOMBRESITIO", "");
 define("LOGO", (RUTA_URL . ""));
+define("LOGOSVG", (RUTA_URL . ""));
+define("EMAIL", "");
 //Versión del sitio
-define("VERSION", "Alpha");
+define("VERSION_FRAMEWORK", "Beta");
 //Errores del sitio
 define("ERROR404", RUTA_URL . "Error/404");
 define("ERROR403", RUTA_URL . "Error/403");
 define("ERROR400", RUTA_URL . "Error/400");
 define("DESARROLLO", RUTA_URL . "Error/desarrollo");
+define("MANTENIMIENTO", RUTA_URL . "Error/mantenimiento");
 
 
 /**
@@ -34,11 +43,12 @@ define("DESARROLLO", RUTA_URL . "Error/desarrollo");
  * @param  Int $code Código de error; Ejemplo: 404
  * @return void
  */
-function error($code){
+function error($code)
+{
     $auxUrl = ERROR404;
     switch ($code) {
         case 404:
-           $auxUrl = ERROR404;
+            $auxUrl = ERROR404;
             break;
         case 400:
             $auxUrl = ERROR400;
@@ -56,24 +66,21 @@ function error($code){
  * y finaliza la ejecución
  * @return void
  */
-function desarrollo(){
+function desarrollo()
+{
     die(header("location: " . DESARROLLO));
 }
 
-//Datos de correo
-define("NOMBRECORREO", "");
-
-//Datos de la aplicación
-
 /**
- * Retorna todos los generos
- * @return Array
+ * Redirecciona a la vista con el aviso, de que
+ * se esta en mantenimiento
+ * y finaliza la ejecución
+ * @return void
  */
-function getGeneros(){
-    return array("Masculino", "Femenino");
+function mantenimiento()
+{
+    die(header("location: " . DESARROLLO));
 }
-
-define("MAXNACIMIENTO", date("Y-m-d", strtotime(date("Y-m-d")."- 10 year")));
 
 /**
  * Filtra los datos de entrada
@@ -83,90 +90,224 @@ define("MAXNACIMIENTO", date("Y-m-d", strtotime(date("Y-m-d")."- 10 year")));
  * @param String $string Input HTML
  * @return String
  */
-function filterINPUT($string){
-    if(preg_match("/^<|.<|>$|;/", $string) >= 1){
+function filterINPUT($string)
+{
+    if (preg_match("/^<|.<|>$|;/", $string) >= 1) {
         die(header("location: " . ERROR400));
     }
     return trim($string);
 }
 
-function saveImg($img, $relativePath, $nameImg = null){
-    //Recibimos los atributos de la imagen
-    $tmpImagen = $img['tmp_name'];
-    $tipoImagen = explode("/", $img['type']);
-    $pesoImagen = $img['size'];
+/**
+ * Guarda la imagen al recibirla mediante $_FILES
+ * Si sus dimensiones son mayores de 1280p y el peso es mayor de 100kb, se reduce su peso sin afectar la calidad
+ *
+ * @param  array $imgs $_FILES
+ * @param  string $relativePath Ruta relativa
+ * @param  array $nameImgs Nombres del archivo; Por defecto el lo agrega de manera aleatoria sin sobreescribir un archivo existente. Si agregas un nombre y existe, sobrescribira ese archivo
+ * @return array | false Retorna los nombres de los archivos, en el caso que falle retorna false
+ */
+function saveImg($imgs, $relativePath, $nameImgs = array())
+{
+    $nombreImagenes = array();
+    $save = function ($tmp_name, $type, $size, $nameImg) use ($relativePath, &$nombreImagenes) {
+        //Recibimos los atributos de la imagen
+        $tmpImagen = $tmp_name;
+        $tipoImagen = explode("/", $type);
+        $pesoImagen = $size;
+
+        //Ruta de la carpeta donde se almacena la imagen
+        $destino = RUTA_APP . $relativePath . "/";
+
+        //Establecemos el nombre de la imagen
+        if ($nameImg == null) {
+            do {
+                $nombreImagen = rand(1, 100) * rand(1, 100) . "_thumb." . $tipoImagen[1];
+            } while (file_exists($destino . $nombreImagen));
+        } else {
+            $nombreImagen = explode(".", $nameImg)[0] . "." . $tipoImagen[1];
+        }
+        //Parámetros optimización, resolución máxima permitida
+        $max_ancho = 1280;
+        $max_alto = 900;
+        //Valida es tipo de archivo a subir, solo permite imagenes jpeg, jpg, png y gif
+        if ($tipoImagen[1] == "jpeg" || $tipoImagen[1] == "jpg" || $tipoImagen[1] == "png" || $tipoImagen[1] == "gif" || $tipoImagen[1] == "webp") {
+            $medidasImagen = getimagesize($tmpImagen);
+            if ($medidasImagen[0] < $max_ancho && $pesoImagen < 100000) {
+                move_uploaded_file($tmpImagen, $destino . $nombreImagen);
+            } else {
+                $rutaOriginal = $tmpImagen;
+                if ($tipoImagen[1] == 'jpeg') {
+                    $original = imagecreatefromjpeg($rutaOriginal);
+                } else if ($tipoImagen[1] == 'png') {
+                    $original = imagecreatefrompng($rutaOriginal);
+                } else if ($tipoImagen[1] == 'gif') {
+                    $original = imagecreatefromgif($rutaOriginal);
+                }
+                $ancho = imagesx($original);
+                $alto = imagesy($original);
+
+                list($ancho, $alto) = getimagesize($rutaOriginal);
+
+                $x_ratio = $max_ancho / $ancho;
+                $y_ratio = $max_alto / $alto;
+
+                if (($ancho <= $max_ancho) && ($alto <= $max_alto)) {
+                    $anchoFinal = $ancho;
+                    $altoFinal = $alto;
+                } else if (($x_ratio * $alto) < $max_alto) {
+                    $altoFinal = ceil($x_ratio * $alto);
+                    $anchoFinal = $max_ancho;
+                } else {
+                    $anchoFinal = ceil($y_ratio * $ancho);
+                    $altoFinal = $max_alto;
+                }
+
+                $lienzo = imagecreatetruecolor($anchoFinal, $altoFinal);
+
+                imagecopyresampled($lienzo, $original, 0, 0, 0, 0, $anchoFinal, $altoFinal, $ancho, $alto);
+                if ($tipoImagen[1] == 'jpeg') {
+                    imagejpeg($lienzo, $destino . $nombreImagen);
+                } else if ($tipoImagen[1] == 'png') {
+                    imagepng($lienzo, $destino . $nombreImagen);
+                } else if ($tipoImagen[1] == 'gif') {
+                    imagegif($lienzo, $destino . $nombreImagen);
+                }
+            }
+            $nombreImagenes[] = $nombreImagen;
+        } else {
+            handler(E_ERROR, "Solo se pueden subir imagenes tipo jpeg, jpg, png y gif.", __FILE__, __LINE__);
+        }
+    };
+
+    $withName = function ($key) use ($nameImgs) {
+        return (key_exists($key, $nameImgs) ? $nameImgs[$key] : null);
+    };
+    //Validamos si en un array de files
+    if (is_array($imgs["name"])) {
+        for ($i = 0; $i < count($imgs['name']); $i++) {
+            $save($imgs["tmp_name"][$i], $imgs["type"][$i], $imgs["size"][$i], $withName($i));
+        }
+    } else {
+        $save($imgs["tmp_name"], $imgs["type"], $imgs["size"], $withName(0));
+    }
+
+    return $nombreImagenes;
+}
+
+/**
+ * Guarda el archivo al recibirla con $_FILES
+ *
+ * @param  mixed $file
+ * @param  mixed $relativePath
+ * @param  mixed $name
+ * @return void
+ */
+function saveFile($file, $relativePath, $name = null)
+{
+    //Recibimos los atributos
+    $tmp = $file['tmp_name'];
+    $tipo = explode("/", $file['type']);
 
     //Ruta de la carpeta donde se almacena la imagen
     $destino = RUTA_APP . $relativePath;
 
     //Establecemos el nombre de la imagen
-    if($nameImg == null){
+    if ($name == null) {
         do {
-            $nombreImagen = rand(1, 100) * rand(1, 100) . "_thumb." . $tipoImagen[1];
-        } while (file_exists($destino . $nombreImagen));
-    }else{
-        $nombreImagen = explode(".", $nameImg)[0] . "." . $tipoImagen[1];
-    }
-    //Parámetros optimización, resolución máxima permitida
-    $max_ancho = 1280;
-    $max_alto = 900;
-    //Valida es tipo de archivo a subir, solo permite imagenes jpeg, jpg, png y gif
-    if ($tipoImagen[1] == "jpeg" || $tipoImagen[1] == "jpg" || $tipoImagen[1] == "png" || $tipoImagen[1] == "gif") {
-        $medidasImagen = getimagesize($tmpImagen);
-        if ($medidasImagen[0] < 1280 && $pesoImagen < 100000) {
-            move_uploaded_file($tmpImagen, $destino . $nombreImagen);
-        } else {
-            $rutaOriginal = $tmpImagen;
-            if ($tipoImagen[1] == 'jpeg') {
-                $original = imagecreatefromjpeg($rutaOriginal);
-            } else if ($tipoImagen[1] == 'png') {
-                $original = imagecreatefrompng($rutaOriginal);
-            } else if ($tipoImagen[1] == 'gif') {
-                $original = imagecreatefromgif($rutaOriginal);
-            }
-            $ancho = imagesx($original);
-            $alto = imagesy($original);
-
-            list($ancho, $alto) = getimagesize($rutaOriginal);
-
-            $x_ratio = $max_ancho / $ancho;
-            $y_ratio = $max_alto / $alto;
-
-            if (($ancho <= $max_ancho) && ($alto <= $max_alto)) {
-                $anchoFinal = $ancho;
-                $altoFinal = $alto;
-            } else if (($x_ratio * $alto) < $max_alto) {
-                $altoFinal = ceil($x_ratio * $alto);
-                $anchoFinal = $max_ancho;
-            } else {
-                $anchoFinal = ceil($y_ratio * $ancho);
-                $altoFinal = $max_alto;
-            }
-
-            $lienzo = imagecreatetruecolor($anchoFinal, $altoFinal);
-
-            imagecopyresampled($lienzo, $original, 0, 0, 0, 0, $anchoFinal, $altoFinal, $ancho, $alto);
-            if ($tipoImagen[1] == 'jpeg') {
-                imagejpeg($lienzo, $destino . $nombreImagen);
-            } else if ($tipoImagen[1] == 'png') {
-                imagepng($lienzo, $destino . $nombreImagen);
-            } else if ($tipoImagen[1] == 'gif') {
-                imagegif($lienzo, $destino . $nombreImagen);
-            }
-        }
-        return $nombreImagen;
+            $nombre = rand(1, 100) * rand(1, 100) . "." . $tipo[1];
+        } while (file_exists($destino . $nombre));
     } else {
-        return "Solo se pueden subir imagenes tipo jpeg, jpg, png y gif.";
+        $nombre = explode(".", $name)[0] . "." . $tipo[1];
+    }
+    if (!move_uploaded_file($tmp, $destino . $nombre)) {
+        return false;
+    }
+    return $nombre;
+}
+
+/**
+ * Suspende el acceso a la plataforma cuando
+ * @var bool $isForced true no exista la $_SESSION['User']; false suspende el acceso total
+ * @return void
+ */
+function suspend($isForced = true)
+{
+    $suspend = function () {
+        if (!key_exists("suspend", $_SESSION)) {
+            $_SESSION["suspend"] = true;
+            mantenimiento();
+        } else {
+            unset($_SESSION["suspend"]);
+        }
+    };
+    if ($isForced) {
+        if (!key_exists("User", $_SESSION)) {
+            $suspend();
+        }
+    } else {
+        $suspend();
     }
 }
 
 
+/**
+ * Valida el token de recaptcha v3 de Google
+ * 
+ * Los valores son obtenidos mediante $_POST -> token, action
+ *
+ * @param  string $typeAction Tipo de la acción
+ * @return bool Si es mayor el score 0.5 y es el mismo tipo de acción return true, en el caso contrario false
+ */
+function validarToken($typeAction)
+{
+    $token = $_POST["token"];
+    $action = $_POST["action"];
+    $cu = curl_init();
+    curl_setopt($cu, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+    curl_setopt($cu, CURLOPT_POST, 1);
+    curl_setopt($cu, CURLOPT_POSTFIELDS, http_build_query(array("secret" => CLAVE_RECAPTCHA_V3, "response" => $token)));
+    curl_setopt($cu, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($cu);
+    curl_close($cu);
 
-function getDateInSpanish($fecha = null){
-    $fecha == null ?: $fecha = date('d-m-Y');
-    setlocale(LC_TIME, 'spanish');
-    $mes=strftime("%B",mktime(0, 0, 0, date('m',strtotime($fecha)), 1, 2000)); 
-    return date('d',strtotime($fecha))." de ".$mes." de ".date('Y',strtotime($fecha));
+    $datos = json_decode($response, true);
+    if ($datos["success"] && $datos["score"] > 0.5) {
+        if ($datos["action"] == $typeAction) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
 
-?>
+/**
+ * Obtener la fecha en español
+ *
+ * @param  string $date default 'now'
+ * @return string fecha en español
+ */
+function getDateInSpanish($date = 'now')
+{
+    $dateTimeObj = new DateTime($date, new DateTimeZone(date_default_timezone_get()));
+    $dateFormatted =
+        IntlDateFormatter::formatObject(
+            $dateTimeObj,
+            "d 'de' MMMM 'de' y",
+            'es_ES'
+        );
+    return $dateFormatted;
+}
+
+/**
+ * Si es null lo convierte en 0
+ *
+ * @param  int $value
+ * @return int
+ */
+function nullToZero($value)
+{
+    return ($value == null ? 0 : $value);
+}
---
+++ Config/configurar.user.php
@@ -0,0 +1,9 @@
+<?php
+//Configuración del usuario
+//Aqui puedes agregar las configuraciones de la plataforma/proyecto
+//Ejemplo
+function getEstados()
+{
+    return array("Inactivo", "Activo");
+}
+?>
