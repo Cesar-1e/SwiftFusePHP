c23b76b3a2b66b752fe6c51a1d6f6496e6066c90
Cesar 1e <cesar.augusto9782@gmail.com>
2024-03-13 05:59:53 -0500

    Version 0.9.3

--- Config/configurar.php
+++ Config/configurar.php
@@ -49,7 +49,7 @@ function is_ssl($bool)
  *
  * @var string
  */
-define("VERSION_SWIFTFUSEPHP", "0.9.2");
+define("VERSION_SWIFTFUSEPHP", "0.9.3");
 
 /**
  * Redirecciona a la vista con su código de estado y finaliza la ejecución.
